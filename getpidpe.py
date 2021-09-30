81f10fbc2d066ca20144dfc0cb2f3b53d55a3382
KietNA <54875703+KietNA-HPT@users.noreply.github.com>
2021-09-30 21:46:06 +0700

    Update getpidpe.py

--- getpidpe.py
+++ getpidpe.py
@@ -47,7 +47,37 @@ def getEventfromSourceID(src_id):
 			Query = parseData['data'][x]['e']['query']
 			print("""[*] Headers: \r\n X-forwarded-for: %s \r\n x-forwarded-port: %s \r\n x-forwarded-proto: %s \r\n Referer: %s \r\n User-Agent: %s \r\n[*] Path: %s \r\n[*] Query: %s \r\n"""
 			% (X_Forward_For,X_Forward_Port,X_Forward_Proto,Referer,User_Agent,Path,Query))
+
+def deleteSourcefromID(src_id):
+	url = "https://api.pipedream.com/v1/sources/%s" % (src_id)
+	r = requests.delete(url, headers=header)
+	if "record not found" not in r.text:
+		print("[*] Source ID %s Delete Success!" % (src_id))
+
+def createSource():
+	name = sys.argv[1]
+	url = "https://api.pipedream.com/v1/sources"
+	component_url = "https://github.com/PipedreamHQ/pipedream/blob/master/components/rss/sources/new-item-in-feed/new-item-in-feed.js"
+	data = {"component_url":component_url,"name":name,"configured_props":{"url":"https://rss.m.pipedream.net","timer":{"intervalSeconds":60}}}
+	r = requests.post(url, data=json.dumps(data), headers=header)
+	print(r.text)
+	if r.status_code == 200:
+		parseData = json.loads(r.text)
+		SourceID = parseData['data']['id']
+		SourceName = parseData['data']['name']
+		URL = parseData['data']['configured_props']['url']
+		active = parseData['data']['active']
+		createdat = parseData['data']['created_at']
+		updated_at = parseData['data']['updated_at']
+		print("""[*] Source %s Created Success: \r\n SourceName: %s \r\n URL: %s \r\n Active: %s \r\n Created At: %s \r\n Update At: %s \r\n""" 
+		% (SourceID,SourceName,URL,active,createdat,updated_at))
+		
 if __name__ == '__main__':
 	getUserfromApiKey()
 	sourceid = getSourcefromUser()
-	getEventfromSourceID(sourceid)
+	if sys.argv[1] == 'GetEvent':
+		getEventfromSourceID(sourceid)
+	elif sys.argv[1] == 'DelSource':
+		deleteSourcefromID(sourceid)
+	elif sys.argv[1] == 'CreateSource':
+		createSource()
