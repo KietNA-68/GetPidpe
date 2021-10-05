2d4090c1b917a17fa73df07e134f329d5d54e497
KietNA <54875703+KietNA-HPT@users.noreply.github.com>
2021-10-05 18:17:02 +0700

    Update getpidpe.py

--- getpidpe.py
+++ getpidpe.py
@@ -30,23 +30,25 @@ def getSourcefromUser():
 
 def getEventfromSourceID(src_id):
 	url = "https://api.pipedream.com/v1/sources/%s/events" % (src_id)
-	r = requests.get(url, headers=header)
-	if r.status_code == 200:
-		parseData = json.loads(r.text)
-		print("[*] Request from %s: \r\n" % (parseData['data'][0]['e']['headers']['host']))
-		for x in range(len(parseData['data'])):
-			X_Forward_For = parseData['data'][x]['e']['headers']['x-forwarded-for']
-			X_Forward_Proto = parseData['data'][x]['e']['headers']['x-forwarded-proto']
-			X_Forward_Port = parseData['data'][x]['e']['headers']['x-forwarded-port']
-			User_Agent = parseData['data'][x]['e']['headers']['user-agent']
-			if 'referer' in parseData['data'][x]['e']['headers']:
-				Referer = parseData['data'][x]['e']['headers']['referer']
-			else:
-				Referer = 'None'
-			Path = parseData['data'][x]['e']['path']
-			Query = parseData['data'][x]['e']['query']
-			print("""[*] Headers: \r\n X-forwarded-for: %s \r\n x-forwarded-port: %s \r\n x-forwarded-proto: %s \r\n Referer: %s \r\n User-Agent: %s \r\n[*] Path: %s \r\n[*] Query: %s \r\n"""
-			% (X_Forward_For,X_Forward_Port,X_Forward_Proto,Referer,User_Agent,Path,Query))
+	while 1:
+		input("$ Press enter to refresh")
+		r = requests.get(url, headers=header)
+		if r.status_code == 200:
+			parseData = json.loads(r.text)
+			print("[*] Request from %s: \r\n" % (parseData['data'][0]['e']['headers']['host']))
+			for x in range(len(parseData['data'])):
+				X_Forward_For = parseData['data'][x]['e']['headers']['x-forwarded-for']
+				X_Forward_Proto = parseData['data'][x]['e']['headers']['x-forwarded-proto']
+				X_Forward_Port = parseData['data'][x]['e']['headers']['x-forwarded-port']
+				User_Agent = parseData['data'][x]['e']['headers']['user-agent']
+				if 'referer' in parseData['data'][x]['e']['headers']:
+					Referer = parseData['data'][x]['e']['headers']['referer']
+				else:
+					Referer = 'None'
+				Path = parseData['data'][x]['e']['path']
+				Query = parseData['data'][x]['e']['query']
+				print("""[*] Headers: \r\n X-forwarded-for: %s \r\n x-forwarded-port: %s \r\n x-forwarded-proto: %s \r\n Referer: %s \r\n User-Agent: %s \r\n[*] Path: %s \r\n[*] Query: %s \r\n"""
+				% (X_Forward_For,X_Forward_Port,X_Forward_Proto,Referer,User_Agent,Path,Query))
 
 def deleteSourcefromID(src_id):
 	url = "https://api.pipedream.com/v1/sources/%s" % (src_id)
